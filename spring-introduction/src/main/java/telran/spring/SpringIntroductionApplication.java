4b6178ed4cd970185b15202a36429dab5a88a8c3
Daniel Kleyman <denny_klm1@yahoo.com>
2023-12-10 18:42:16 +0200

    HW-52

---
+++ spring-introduction/src/main/java/telran/spring/SpringIntroductionApplication.java
@@ -0,0 +1,14 @@
+package telran.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroductionApplication.class, args);
+	}
+
+}
+//http://localhost:8080/calculator/add?op1=5&op2=3
